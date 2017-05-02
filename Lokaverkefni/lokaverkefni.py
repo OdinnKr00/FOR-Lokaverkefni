c961ed5db7b9c0929fc8d3d50dfb0d5520e99712
Ólafur Eysteinn Eysteinsson <olafurey298@nemi.tskoli.is>
2017-05-02 15:15:37 +0000

    Add files via upload
    hrutarspilið virkar :) þarf að kommenta

--- Lokaverkefni/lokaverkefni.py
+++ Lokaverkefni/lokaverkefni.py
@@ -1,10 +1,11 @@
 # Hóp/Lokaverkefni - Ólafur Eysteinn Eysteinsson og Óðinn Kristjánsson - 24. Apríl 2017
 import random
-import time
 stokkur = []
 leikmadur1 = []
 leikmadur2 = []
-skra = open("hrutar.txt", "r")
+jafntefli = []
+svar = 1
+skra = open("hrutarprufa.txt", "r")
 print("Hér eru öll spilin:")
 spil = skra.read().split("\n")
 skra.close()
@@ -26,10 +27,17 @@ for x in stokkur:
         print("Villa!")
 random.shuffle(leikmadur1)
 random.shuffle(leikmadur2)
-svar = input("Viltu byrja? J fyrir Já ")
 
-while svar == "j" or svar == "J":
+while svar == 1:
     for lota in range(1,10000):
+        if lota>1:
+            svar = input("Næsta lota? *ýttu á Enter* ")
+        if len(leikmadur1)==0:
+            print("ÞÚ TAPAÐIR!")
+            break
+        elif len(leikmadur2)==0:
+            print("ÞÚ VANNST!")
+            break
         print("")
         print("Lota",lota,":")
         for x in leikmadur1:
@@ -38,169 +46,58 @@ while svar == "j" or svar == "J":
         for x in leikmadur2:
             flokkar2 = x.split(" ")
             break
-        #for i in flokkar2:
-           # if i.isdigit() == True or i.replace(".", "", 1).isdigit() == True:
-               # print(i)
         print("Það eru",len(leikmadur1),"spil í stokknum þínum og",len(leikmadur2),"spil í stokk leikmanns 2.")
-        print("Þetta er hrúturinn þinn í þessari lotu:",flokkar1)
+        print("Þetta er hrúturinn þinn í þessari lotu:")
+        print("Nafn:",flokkar1[0])
+        print("1: Þyndgd í kílóum =",flokkar1[1],"kg.")
+        print("2: Mjólkuragni dætra =",flokkar1[2])
+        print("3: Einkunn ullar =",flokkar1[3])
+        print("4: Fjöldi afkvæma =",flokkar1[4])
+        print("5: Einkunn læris =",flokkar1[5])
+        print("6: Frjosemi =",flokkar1[6])
+        print("7: Gerð/Þykkt bakvöðva =",flokkar1[7])
+        print("8: Einkunn fyrir malir =",flokkar1[8])
         if lota%2!=0:
             print("Þú velur flokk!")
-            print("Viltu nota Þyndgd í kílóum, Mjólkuragni dætra, Einkunn ullar, Fjöldi afkvæma"
-                  ", Einkunn læris, Frjosemi, Gerð/Þykkt bakvöðva eða Einkunn fyrir malir")
+
             val = int(input("Hvaða flokk viltu nota? 1, 2, 3, 4, 5, 6, 7 eða 8 "))
         else:
-            print("Talvan velur flokk!")
+            print("Leikmaður 2 velur flokk!")
             val=random.randint(1,8)
-            print("Talvan valdi flokk nr.",val)
-        if val == 1:
-            if float(flokkar1[1]) > float(flokkar2[1]):
-                print(flokkar1[0],"vann með",flokkar1[1],"á móti",flokkar2[0],"sem var með",flokkar2[1])
-                print("Þú vannst,",flokkar2[0],"fer neðst í stokkinn þinn!")
-                flokkar1=" ".join(flokkar1)
-                flokkar2=" ".join(flokkar2)
-                leikmadur1.append(flokkar1)
-                leikmadur1.append(flokkar2)
-            elif float(flokkar1[1]) < float(flokkar2[1]):
-                print(flokkar2[0],"vann með",flokkar2[1],"á móti",flokkar1[0],"sem var með",flokkar1[1])
-                print("Talvan vann,", flokkar1[0], "fer neðst í stokkinn hans!")
-                flokkar1=" ".join(flokkar1)
-                flokkar2=" ".join(flokkar2)
-                leikmadur2.append(flokkar1)
-                leikmadur2.append(flokkar2)
-            elif float(flokkar1[1]) > float(flokkar2[1]):
-                print("Jafntefli")
-            del leikmadur1[0]
-            del leikmadur2[0]
-        elif val == 2:
-            if float(flokkar1[2]) > float(flokkar2[2]):
-                print(flokkar1[0],"vann með",flokkar1[2],"á móti",flokkar2[0],"sem var með",flokkar2[2])
-                print("Þú vannst,",flokkar2[0],"fer neðst í stokkinn þinn!")
-                flokkar1=" ".join(flokkar1)
-                flokkar2=" ".join(flokkar2)
-                leikmadur1.append(flokkar1)
-                leikmadur1.append(flokkar2)
-            elif float(flokkar1[2]) < float(flokkar2[2]):
-                print(flokkar2[0],"vann með",flokkar2[2],"á móti",flokkar1[0],"sem var með",flokkar1[2])
-                print("Talvan vann,", flokkar1[0], "fer neðst í stokkinn hans!")
-                flokkar1=" ".join(flokkar1)
-                flokkar2=" ".join(flokkar2)
-                leikmadur2.append(flokkar1)
-                leikmadur2.append(flokkar2)
-            elif float(flokkar1[2]) > float(flokkar2[2]):
-                print("Jafntefli")
-            del leikmadur1[0]
-            del leikmadur2[0]
-        elif val == 3:
-            if float(flokkar1[3]) > float(flokkar2[3]):
-                print(flokkar1[0],"vann með",flokkar1[3],"á móti",flokkar2[0],"sem var með",flokkar2[3])
-                print("Þú vannst,",flokkar2[0],"fer neðst í stokkinn þinn!")
-                flokkar1=" ".join(flokkar1)
-                flokkar2=" ".join(flokkar2)
-                leikmadur1.append(flokkar1)
-                leikmadur1.append(flokkar2)
-            elif float(flokkar1[2]) < float(flokkar2[3]):
-                print(flokkar1[0],"vann með",flokkar1[3],"á móti",flokkar2[0],"sem var með",flokkar2[3])
-                print("Talvan vann,", flokkar1[0], "fer neðst í stokkinn hans!")
-                flokkar1=" ".join(flokkar1)
-                flokkar2=" ".join(flokkar2)
-                leikmadur2.append(flokkar1)
-                leikmadur2.append(flokkar2)
-            elif float(flokkar1[3]) > float(flokkar2[3]):
-                print("Jafntefli")
-            del leikmadur1[0]
-            del leikmadur2[0]
-        elif val == 4:
-            if float(flokkar1[4]) > float(flokkar2[4]):
-                print(flokkar1[0],"vann með",flokkar1[4],"á móti",flokkar2[0],"sem var með",flokkar2[4])
-                print("Þú vannst,",flokkar2[0],"fer neðst í stokkinn þinn!")
-                flokkar1=" ".join(flokkar1)
-                flokkar2=" ".join(flokkar2)
-                leikmadur1.append(flokkar1)
-                leikmadur1.append(flokkar2)
-            elif float(flokkar1[4]) < float(flokkar2[4]):
-                print(flokkar1[0],"vann með",flokkar1[4],"á móti",flokkar2[0],"sem var með",flokkar2[4])
-                print("Talvan vann,", flokkar1[0], "fer neðst í stokkinn hans!")
-                flokkar1=" ".join(flokkar1)
-                flokkar2=" ".join(flokkar2)
-                leikmadur2.append(flokkar1)
-                leikmadur2.append(flokkar2)
-            elif float(flokkar1[4]) > float(flokkar2[4]):
-                print("Jafntefli")
-            del leikmadur1[0]
-            del leikmadur2[0]
-        elif val == 5:
-            if float(flokkar1[5]) > float(flokkar2[5]):
-                print(flokkar1[0],"vann með",flokkar1[5],"á móti",flokkar2[0],"sem var með",flokkar2[5])
-                print("Þú vannst,",flokkar2[0],"fer neðst í stokkinn þinn!")
-                flokkar1=" ".join(flokkar1)
-                flokkar2=" ".join(flokkar2)
-                leikmadur1.append(flokkar1)
-                leikmadur1.append(flokkar2)
-            elif float(flokkar1[5]) < float(flokkar2[5]):
-                print(flokkar1[0],"vann með",flokkar1[5],"á móti",flokkar2[0],"sem var með",flokkar2[5])
-                print("Talvan vann,", flokkar1[0], "fer neðst í stokkinn hans!")
-                flokkar1=" ".join(flokkar1)
-                flokkar2=" ".join(flokkar2)
-                leikmadur2.append(flokkar1)
-                leikmadur2.append(flokkar2)
-            elif float(flokkar1[5]) > float(flokkar2[5]):
-                print("Jafntefli")
-            del leikmadur1[0]
-            del leikmadur2[0]
-        elif val == 6:
-            if float(flokkar1[6]) > float(flokkar2[6]):
-                print(flokkar1[0],"vann með",flokkar1[6],"á móti",flokkar2[0],"sem var með",flokkar2[6])
-                print("Þú vannst,",flokkar2[0],"fer neðst í stokkinn þinn!")
-                flokkar1=" ".join(flokkar1)
-                flokkar2=" ".join(flokkar2)
-                leikmadur1.append(flokkar1)
-                leikmadur1.append(flokkar2)
-            elif float(flokkar1[6]) < float(flokkar2[6]):
-                print(flokkar1[0],"vann með",flokkar1[6],"á móti",flokkar2[0],"sem var með",flokkar2[6])
-                print("Talvan vann,", flokkar1[0], "fer neðst í stokkinn hans!")
-                flokkar1=" ".join(flokkar1)
-                flokkar2=" ".join(flokkar2)
-                leikmadur2.append(flokkar1)
-                leikmadur2.append(flokkar2)
-            elif float(flokkar1[6]) > float(flokkar2[6]):
-                print("Jafntefli")
-            del leikmadur1[0]
-            del leikmadur2[0]
-        elif val == 7:
-            if float(flokkar1[7]) > float(flokkar2[7]):
-                print(flokkar1[0],"vann með",flokkar1[7],"á móti",flokkar2[0],"sem var með",flokkar2[7])
-                print("Þú vannst,",flokkar2[0],"fer neðst í stokkinn þinn!")
-                flokkar1=" ".join(flokkar1)
-                flokkar2=" ".join(flokkar2)
-                leikmadur1.append(flokkar1)
-                leikmadur1.append(flokkar2)
-            elif float(flokkar1[7]) < float(flokkar2[7]):
-                print(flokkar1[0],"vann með",flokkar1[7],"á móti",flokkar2[0],"sem var með",flokkar2[7])
-                print("Talvan vann,", flokkar1[0], "fer neðst í stokkinn hans!")
-                flokkar1=" ".join(flokkar1)
-                flokkar2=" ".join(flokkar2)
-                leikmadur2.append(flokkar1)
-                leikmadur2.append(flokkar2)
-            elif float(flokkar1[7]) > float(flokkar2[7]):
-                print("Jafntefli")
-            del leikmadur1[0]
-            del leikmadur2[0]
-        elif val == 8:
-            if float(flokkar1[8]) > float(flokkar2[8]):
-                print(flokkar1[0],"vann með",flokkar1[8],"á móti",flokkar2[0],"sem var með",flokkar2[8])
-                print("Þú vannst,",flokkar2[0],"fer neðst í stokkinn þinn!")
-                flokkar1=" ".join(flokkar1)
-                flokkar2=" ".join(flokkar2)
-                leikmadur1.append(flokkar1)
-                leikmadur1.append(flokkar2)
-            elif float(flokkar1[8]) < float(flokkar2[8]):
-                print(flokkar1[0],"vann með",flokkar1[8],"á móti",flokkar2[0],"sem var með",flokkar2[8])
-                print("Talvan vann,", flokkar1[0], "fer neðst í stokkinn hans!")
-                flokkar1=" ".join(flokkar1)
-                flokkar2=" ".join(flokkar2)
-                leikmadur2.append(flokkar1)
-                leikmadur2.append(flokkar2)
-            elif float(flokkar1[8]) > float(flokkar2[8]):
-                print("Jafntefli")
-            del leikmadur1[0]
-            del leikmadur2[0]
+            print("Leikmaður 2 valdi flokk nr.",val)
+        print("")
+        if float(flokkar1[val]) > float(flokkar2[val]):
+            print("Þú vannst,", flokkar2[0], "fer neðst í stokkinn þinn!")
+            print(flokkar1[0], "vann með", flokkar1[val], "á móti", flokkar2[0], "sem var með", flokkar2[val])
+            if len(jafntefli) > 0:
+                print("Þú fékkst líka", jafntefli)
+                for j in range(len(jafntefli)):
+                    print(jafntefli[j])
+                    leikmadur1.append(jafntefli[j])
+                list.clear(jafntefli)
+            flokkar1 = " ".join(flokkar1)
+            flokkar2 = " ".join(flokkar2)
+            leikmadur1.append(flokkar1)
+            leikmadur1.append(flokkar2)
+        elif float(flokkar1[val]) < float(flokkar2[val]):
+            print("Leikmaður 2 vann,", flokkar1[0], "fer neðst í stokkinn hans!")
+            print(flokkar2[0], "vann með", flokkar2[val], "á móti", flokkar1[0], "sem var með", flokkar1[val])
+            if len(jafntefli) > 0:
+                print("Leikmaður 2 fékk líka:")
+                for j in range(len(jafntefli)):
+                    print(jafntefli[j])
+                    leikmadur2.append(jafntefli[j])
+                list.clear(jafntefli)
+            flokkar1 = " ".join(flokkar1)
+            flokkar2 = " ".join(flokkar2)
+            leikmadur2.append(flokkar1)
+            leikmadur2.append(flokkar2)
+        elif float(flokkar1[val]) == float(flokkar2[val]):
+            print("Jafntefli!", flokkar1[0], "og", flokkar2[0], "voru með jafnmikið.")
+            flokkar1 = " ".join(flokkar1)
+            flokkar2 = " ".join(flokkar2)
+            jafntefli.append(flokkar1)
+            jafntefli.append(flokkar2)
+        del leikmadur1[0]
+        del leikmadur2[0]
+        print("Spilin í pottinum:",jafntefli)
